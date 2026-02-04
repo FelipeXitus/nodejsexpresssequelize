9044e9ef46dc2e5749306b9c55e24d93b1835ef4
Felipe Caldeira <felipe.xitus@gmail.com>
2026-02-04 08:44:45 -0300

    feat: update .gitignore and add Sequelize models and migrations for Pessoa, Categoria, Curso, Matricula

---
+++ src/migrations/20260204110554-create-curso.js
@@ -0,0 +1,54 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('cursos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      titulo: {
+        type: Sequelize.STRING
+      },
+      descricao: {
+        type: Sequelize.STRING
+      },
+      data_inicio: {
+        type: Sequelize.DATE
+      },
+      categoria_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'categorias',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      docente_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'pessoas',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('cursos');
+  }
+};
---
+++ src/models/curso.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Curso extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Curso.init({
+    titulo: DataTypes.STRING,
+    descricao: DataTypes.STRING,
+    data_inicio: DataTypes.DATE,
+    categoria_id: DataTypes.INTEGER,
+    docente_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Curso',
+    tableName: 'cursos'
+  });
+  return Curso;
+};
